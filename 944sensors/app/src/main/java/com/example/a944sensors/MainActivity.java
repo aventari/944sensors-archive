c607a9f7f4fdf5ce8485796cd10b8ee070210c53
kevin <kevin@agcolo.com>
2023-04-17 13:55:48 -0700

    Old android apps for interfacing with ble sensors. All deprecated
    Onto flutter now in other repo, search for it

---
+++ 944sensors/app/src/main/java/com/example/a944sensors/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a944sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user touches Scan BLE button
+     */
+    public void onClickBleScan(View view) {
+    }
+}
